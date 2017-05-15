9cfb336e567d3647cff6288aa7100a2a99b819f7
Harsh Vardhan <harsh59v@gmail.com>
2017-05-15 12:43:56 +0530

    Pic-farmer now puts images in a folder
    WIP : Check for images created in previous instance

--- pic-farmer.py
+++ pic-farmer.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
+import os
 import random
 import sys
 import requests
 
-# import typing
-
 
 def farm_pics(lo, hi, pi):
-    '''
-    Ensures that *pi* number of images are created and stored in
-    the current directory.
+    '''Ensures that *pi* number of images are created and stored in the current directory.
 
     :lo lower bound of picture dimension
     :hi higer bound of picture dimension
     :pi number of pics to be downloaded
     '''
     rnd = random.randint
+    if os.path.isfile('runinfo.txt'):
+        with open('runinfo.txt', 'w') as f:   # Adds previous info about, in the root of the repo.
+            f.write("{},{},{}\n".format(lo, hi, pi))
+
+    if not os.path.isdir('sample_images'):
+        os.makedirs('sample_images')
+
+    os.chdir('sample_images')
 
     for i in range(pi):
         L, H = rnd(lo, hi), rnd(lo, hi)
         x = requests.get('https://placekitten.com/g/' + str(L) + '/' + str(H))
         assert(x.status_code == 200), "Couldn't reach placeKitten in hit{}\n".format(i)
- 
+
         if x.text is '':  # If image couldn't be served, get a boring image
             x = requests.get('https://placehold.it/' + str(L) + 'x' + str(H))
             assert(x.status_code == 200), "Couldn't reach placeHold in hit{}\n".format(i)
